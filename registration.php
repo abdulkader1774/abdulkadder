5dbb23dbf8d426299045f58030a0b5b69741ddcf
Abdul Kader Jilani <abdulkaderq1774@gmail.com>
2025-08-29 17:50:36 +0600

    re

--- registration.php
+++ registration.php
@@ -68,18 +68,113 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                                 <label for="username" class="form-label">Username</label>
                                 <input type="text" class="form-control" id="username" name="username" required>
                             </div>
-                            <div class="mb-3">
+                            <div class=" d-flex">
+                                <div class="name english">
+                                    <label for="name-english" class="form-label">Name(English)</label>
+                                    <input type="text" class="form-control" id="nameenglish" name="nameenglish" required>
+                                </div>
+                                    <div class="name bangla">
+                                    <label for="name-bangla" class="form-label">Name(Bangla)</label>
+                                    <input type="text" class="form-control" id="namebangla" name="namebangla" required>
+                                </div>
+                            </div>
+
+                            <div class=" d-flex">
+                                <div class="email">
+                                    <label for="email" class="form-label">Email</label>
+                                    <input type="text" class="form-control" id="email" name="email" required>
+                                </div>
+                                    <div class="phone">
+                                    <label for="phone" class="form-label">Phone</label>
+                                    <input type="text" class="form-control" id="phone" name="phone" required>
+                                </div>
+                            </div>
+
+                            <div class=" d-flex">
+                                <div class="gender">
+                                    <label for="gender" class="form-label">Gender</label>
+                                    <input type="text" class="form-control" id="gender" name="gender" required>
+                                </div>
+                                    <div class="dateofbirth">
+                                    <label for="dateofbirth" class="form-label">Date of Birth</label>
+                                    <input type="text" class="form-control" id="dateofbirth" name="dateofbirth" required>
+                                </div>
+                            </div>
+
+                            <div class=" d-flex">
+                                <div class="institute-english">
+                                    <label for="institute-english" class="form-label">Institute(English)</label>
+                                    <input type="text" class="form-control" id="institute-english" name="institute-english" required>
+                                </div>
+                                    <div class="institute-bangla">
+                                    <label for="institute-bangla" class="form-label">Institute(Bangla)</label>
+                                    <input type="text" class="form-control" id="institute-bangla" name="institute-bangla" required>
+                                </div>
+                            </div>
+
+                            
+                            <div class=" d-flex">
+                                <div class="medium">
+                                    <label for="medium" class="form-label">Medium</label>
+                                    <input type="text" class="form-control" id="medium" name="medium" required>
+                                </div>
+
+                                <div class="class">
+                                    <label for="class" class="form-label">Class</label>
+                                    <input type="text" class="form-control" id="class" name="class" required>
+                                </div>
+
+                                <div class="catagori">
+                                    <label for="catagori" class="form-label">Catagori</label>
+                                    <input type="select" class="form-control" id="catagori" name="catagori" required>
+                                    <select name="data" id="">
+                                        <option value="1">hello</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class=" d-flex">
+
+                            
+                                <div class="division">
+                                    <label for="division" class="form-label">Division</label>
+                                    <input type="text" class="form-control" id="division" name="division" required>
+                                </div>
+
+                                <div class="district">
+                                    <label for="district" class="form-label">District</label>
+                                    <input type="text" class="form-control" id="district" name="district" required>
+                                </div>
+
+
+                                <div class="upozila">
+                                    <label for="upozila" class="form-label">Upozila</label>
+                                    <input type="text" class="form-control" id="upozila" name="upozila" required>
+                                </div>
+                            </div>
+
+                            <div class=" d-flex">
+                                <div class="mb-3">
                                 <label for="password" class="form-label">Password</label>
                                 <input type="password" class="form-control" id="password" name="password" required>
                             </div>
+
                             <div class="mb-3">
+                                <label for="confiram-password" class="form-label">Confirm Password</label>
+                                <input type="password" class="form-control" id="confiram-password" name="confiram-password" required>
+                            </div>
+                            </div>
+
+                            <!-- <div class="mb-3">
                                 <label for="email" class="form-label">Email</label>
                                 <input type="email" class="form-control" id="email" name="email" required>
-                            </div>
-                            <div class="mb-3">
+                            </div> -->
+
+                            <!-- <div class="mb-3">
                                 <label for="full_name" class="form-label">Full Name</label>
                                 <input type="text" class="form-control" id="full_name" name="full_name">
-                            </div>
+                            </div> -->
+
                             <button type="submit" class="btn btn-primary w-100">Register</button>
                         </form>
                         
